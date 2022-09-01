11d468c2979f5c0439c6ccb76937aa0a9539b4c2
av.litvin <av.litvin73@gmail.com>
2022-09-01 19:20:51 +0300

    feat: asdfasdf

--- test_capitalize.py
+++ test_capitalize.py
@@ -1,12 +1,6 @@
 from task import capitalize_string
 
 
-print(capitalize_string('asd'))
-def capitalize_string(s):
-    if not isinstance(s, str):
-        raise TypeError('Please provide a string')
-    return s.capitalize()
-
 def test_capitalize_string():
     assert capitalize_string('test') == 'Test'
 
